be935dae71a644eafd9ba1da265c1f94c53a0817
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-03-23 14:06:51 +0200

    done

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Delegate declaration
+public delegate void DisplayMessageDelegate(string message);
+
+// Application class
+public class MessageApp
+{
+    // Method to display message
+    public static void DisplayMessage(string message)
+    {
+        Console.WriteLine("Message: " + message);
+    }
+
+    // Method to display message in uppercase
+    public static void DisplayMessageUppercase(string message)
+    {
+        Console.WriteLine("Message: " + message.ToUpper());
+    }
+
+    // Method to display message in lowercase
+    public static void DisplayMessageLowercase(string message)
+    {
+        Console.WriteLine("Message: " + message.ToLower());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating delegate instance
+        DisplayMessageDelegate showMessageDelegate = MessageApp.DisplayMessage;
+
+        // Test by invoking the delegate with different methods
+        showMessageDelegate("Hello, World!");
+        showMessageDelegate = MessageApp.DisplayMessageUppercase;
+        showMessageDelegate("Hello, World!");
+        showMessageDelegate = MessageApp.DisplayMessageLowercase;
+        showMessageDelegate("Hello, World!");
+    }
+}
