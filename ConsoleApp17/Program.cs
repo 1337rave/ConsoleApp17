8a8eee90905e7015f0e31f72efb66900e0a26d1f
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-03-23 14:31:46 +0200

    done

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Delegate declaration for arithmetic checks
+public delegate bool ArithmeticCheck(int num);
+
+// ArithmeticChecker class
+public class ArithmeticChecker
+{
+    // Method to check if a number is even
+    public static bool IsEven(int num)
+    {
+        return num % 2 == 0;
+    }
+
+    // Method to check if a number is odd
+    public static bool IsOdd(int num)
+    {
+        return num % 2 != 0;
+    }
+
+    // Method to check if a number is prime
+    public static bool IsPrime(int num)
+    {
+        if (num <= 1)
+            return false;
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Method to check if a number is in the Fibonacci sequence
+    public static bool IsFibonacci(int num)
+    {
+        int a = 0, b = 1;
+        while (b < num)
+        {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b == num;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create Predicate delegate instances for different checks
+        Predicate<int> isEvenPredicate = ArithmeticChecker.IsEven;
+        Predicate<int> isOddPredicate = ArithmeticChecker.IsOdd;
+        Predicate<int> isPrimePredicate = ArithmeticChecker.IsPrime;
+        Predicate<int> isFibonacciPredicate = ArithmeticChecker.IsFibonacci;
+
+        // Test arithmetic checks using delegates
+        int num = 8;
+
+        Console.WriteLine($"{num} is even: {isEvenPredicate(num)}");
+        Console.WriteLine($"{num} is odd: {isOddPredicate(num)}");
+        Console.WriteLine($"{num} is prime: {isPrimePredicate(num)}");
+        Console.WriteLine($"{num} is in Fibonacci sequence: {isFibonacciPredicate(num)}");
+    }
+}
